249189e2b32f7868bcae546323ca5440460815f2
Rafael Garcia <regestra64@gmail.com>
2021-01-15 14:23:07 -0600

    Solucionado bug de menu cuando sucede un error

--- src/inventario/telefonos/InventarioTelefonos.java
+++ src/inventario/telefonos/InventarioTelefonos.java
@@ -15,6 +15,7 @@ public class InventarioTelefonos {
         int opcion, filaMatriz = 0;
         String ruta, inventario[][] = new String[100][3], salida;
         JTextArea pantalla = new JTextArea();
+        boolean error = false;
         //Ingreso de la ruta del archivo de texto
         do {
             ruta = JOptionPane.showInputDialog("Bienvenido a su sistema de inventario de telefonos\nIngrese la ruta en donde se encuentra su archivo de inventarios.");
@@ -40,103 +41,108 @@ public class InventarioTelefonos {
                 pantalla.setText(salida);
                 readerb.close();
             } catch (IOException | HeadlessException ex) {
+                error = true;
                 JOptionPane.showMessageDialog(null, "Se ha producido un error\nDescripcion del error:\n" + ex.getMessage());
             }
+            if (!error) {
 
-            //Menu principal del programa
-            opcion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de su opcion a elegir\n1 - Ver inventario general"
-                    + "\n2 - Agregar telefono a inventario\n3 - Vender telefono\nOtro numero para salir"));
-            switch (opcion) {
-                case 1:
-                    JOptionPane.showMessageDialog(null, pantalla);
+                //Menu principal del programa
+                opcion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de su opcion a elegir\n1 - Ver inventario general"
+                        + "\n2 - Agregar telefono a inventario\n3 - Vender telefono\nOtro numero para salir"));
+                switch (opcion) {
+                    case 1:
+                        JOptionPane.showMessageDialog(null, pantalla);
 
-                    break;
-                case 2:
-                    //Menu para agregar stock o agregar nuevo telefono
-                    int opcionAgregar;
-                    do {
-                        opcionAgregar = Integer.parseInt(JOptionPane.showInputDialog("Que desea hacer\n1 - Agregar inventario a un telefono\n2 - Agregar nuevo telefono"));
-                    } while (opcionAgregar < 1 || opcionAgregar > 2);
-                    switch (opcionAgregar) {
-                        case 1:
-                            //Modificar stock en la matriz
-                            int posicion,
-                             precio;
-                            do {
-                                posicion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la posicion del telefono para cambiar su stock"));
-                            } while (posicion < 1 || posicion > filaMatriz);
-                            do {
-                                precio = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el nuveo numero de stock del telefono Marca: " + inventario[posicion - 1][0]));
-                            } while (precio <= 0);
-                            inventario[posicion - 1][2] = "" + precio;
-                            break;
-                        case 2:
-                            //Agregar nuevo registro en la matriz
-                            String nTelefono = "";
-                            double nPrecio;
-                            int nStock;
-                            do {
-                                nTelefono = JOptionPane.showInputDialog("Ingrese el nombre del nuevo telefono");
-                            } while (nTelefono.isEmpty());
-                            inventario[filaMatriz][0] = nTelefono;
-                            do {
-                                nPrecio = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el precio del nuevo telefono\nRecuerde debe ser mayor a cero"));
-                            } while (nPrecio <= 0);
-                            inventario[filaMatriz][1] = "" + nPrecio;
-                            do {
-                                nStock = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el Stock del nuevo telefono\nRecuerde debe ser mayor a cero"));
-                            } while (nStock <= 0);
-                            inventario[filaMatriz][2] = "" + nStock;
-                            filaMatriz++;
-                            break;
-                    }
-                    //modificar el archivo de texto
-                    try {
-                        BufferedWriter writterb = new BufferedWriter(new FileWriter(ruta));
-                        writterb.write("");
-                        for (int i = 0; i < filaMatriz; i++) {
-                            writterb.write(inventario[i][0] + "-" + inventario[i][1] + "-" + inventario[i][2]);
-                            writterb.newLine();
-                        }
-                        writterb.close();
-                    } catch (IOException | HeadlessException e) {
-                        JOptionPane.showMessageDialog(null, "Se ha producido un error\nDescripcion del error:\n" + e.getMessage());
-                    }
-                    break;
-                case 3:
-                    //Vender telefono
-                    //Validar si existe registro de telefonos para evitar bucle infinito
-                    if (filaMatriz > 0) {
-                        int posicion;
-                        //Validar seleccion correcta de telefono
+                        break;
+                    case 2:
+                        //Menu para agregar stock o agregar nuevo telefono
+                        int opcionAgregar;
                         do {
-                            posicion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la posicion del telefono a comprar"));
-                        } while (posicion < 1 || posicion > filaMatriz);
-                        //Validar si se posee stock del telefono seleccionado para evitar stock negativo
-                        if (Integer.parseInt(inventario[posicion - 1][2]) > 0) {
-                            inventario[posicion - 1][2] = (Integer.parseInt(inventario[posicion - 1][2]) - 1) + "";
-                            try {
-                                BufferedWriter writterb = new BufferedWriter(new FileWriter(ruta));
-                                writterb.write("");
-                                for (int i = 0; i < filaMatriz; i++) {
-                                    writterb.write(inventario[i][0] + "-" + inventario[i][1] + "-" + inventario[i][2]);
-                                    writterb.newLine();
+                            opcionAgregar = Integer.parseInt(JOptionPane.showInputDialog("Que desea hacer\n1 - Agregar inventario a un telefono\n2 - Agregar nuevo telefono"));
+                        } while (opcionAgregar < 1 || opcionAgregar > 2);
+                        switch (opcionAgregar) {
+                            case 1:
+                                //Modificar stock en la matriz
+                                int posicion,
+                                 precio;
+                                do {
+                                    posicion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la posicion del telefono para cambiar su stock"));
+                                } while (posicion < 1 || posicion > filaMatriz);
+                                do {
+                                    precio = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el nuveo numero de stock del telefono Marca: " + inventario[posicion - 1][0]));
+                                } while (precio <= 0);
+                                inventario[posicion - 1][2] = "" + precio;
+                                break;
+                            case 2:
+                                //Agregar nuevo registro en la matriz
+                                String nTelefono = "";
+                                double nPrecio;
+                                int nStock;
+                                do {
+                                    nTelefono = JOptionPane.showInputDialog("Ingrese el nombre del nuevo telefono");
+                                } while (nTelefono.isEmpty());
+                                inventario[filaMatriz][0] = nTelefono;
+                                do {
+                                    nPrecio = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el precio del nuevo telefono\nRecuerde debe ser mayor a cero"));
+                                } while (nPrecio <= 0);
+                                inventario[filaMatriz][1] = "" + nPrecio;
+                                do {
+                                    nStock = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el Stock del nuevo telefono\nRecuerde debe ser mayor a cero"));
+                                } while (nStock <= 0);
+                                inventario[filaMatriz][2] = "" + nStock;
+                                filaMatriz++;
+                                break;
+                        }
+                        //modificar el archivo de texto
+                        try {
+                            BufferedWriter writterb = new BufferedWriter(new FileWriter(ruta));
+                            writterb.write("");
+                            for (int i = 0; i < filaMatriz; i++) {
+                                writterb.write(inventario[i][0] + "-" + inventario[i][1] + "-" + inventario[i][2]);
+                                writterb.newLine();
+                            }
+                            writterb.close();
+                        } catch (IOException | HeadlessException e) {
+                            JOptionPane.showMessageDialog(null, "Se ha producido un error\nDescripcion del error:\n" + e.getMessage());
+                        }
+                        break;
+                    case 3:
+                        //Vender telefono
+                        //Validar si existe registro de telefonos para evitar bucle infinito
+                        if (filaMatriz > 0) {
+                            int posicion;
+                            //Validar seleccion correcta de telefono
+                            do {
+                                posicion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la posicion del telefono a comprar"));
+                            } while (posicion < 1 || posicion > filaMatriz);
+                            //Validar si se posee stock del telefono seleccionado para evitar stock negativo
+                            if (Integer.parseInt(inventario[posicion - 1][2]) > 0) {
+                                inventario[posicion - 1][2] = (Integer.parseInt(inventario[posicion - 1][2]) - 1) + "";
+                                try {
+                                    BufferedWriter writterb = new BufferedWriter(new FileWriter(ruta));
+                                    writterb.write("");
+                                    for (int i = 0; i < filaMatriz; i++) {
+                                        writterb.write(inventario[i][0] + "-" + inventario[i][1] + "-" + inventario[i][2]);
+                                        writterb.newLine();
+                                    }
+                                    writterb.close();
+                                } catch (IOException | HeadlessException e) {
+                                    JOptionPane.showMessageDialog(null, "Se ha producido un error\nDescripcion del error:\n" + e.getMessage());
                                 }
-                                writterb.close();
-                            } catch (IOException | HeadlessException e) {
-                                JOptionPane.showMessageDialog(null, "Se ha producido un error\nDescripcion del error:\n" + e.getMessage());
+                            } else {
+                                JOptionPane.showMessageDialog(null, "El telefono seleccionado no cuenta con stock por el momento");
                             }
                         } else {
-                            JOptionPane.showMessageDialog(null, "El telefono seleccionado no cuenta con stock por el momento");
+                            JOptionPane.showMessageDialog(null, "No se encuentran registros de inventario");
                         }
-                    } else {
-                        JOptionPane.showMessageDialog(null, "No se encuentran registros de inventario");
-                    }
-                    break;
-                default:
-                    //Saludo de despedida para indicar en donde se guarda el documento de texto
-                    JOptionPane.showMessageDialog(null, "Saliendo del programa de inventarios\nSu informacion esta a salvo en un archivo de texto en la siguiente direccion:\n"
-                            + ruta);
+                        break;
+                    default:
+                        //Saludo de despedida para indicar en donde se guarda el documento de texto
+                        JOptionPane.showMessageDialog(null, "Saliendo del programa de inventarios\nSu informacion esta a salvo en un archivo de texto en la siguiente direccion:\n"
+                                + ruta);
+                }
+            } else {
+                opcion = 5;
             }
         } while (opcion >= 1 && opcion <= 3);
 
